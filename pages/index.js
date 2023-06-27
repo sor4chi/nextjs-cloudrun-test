ff8b47624c969608c0f58690dad32aaa16017c9c
sor4chi <sorakawa0218@gmail.com>
2023-06-27 10:33:24 +0900

    feat: add rendered-time to top page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -14,8 +14,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
         </h1>
 
+        <p className={styles.description}>{new Date().toLocaleString()}</p>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,10 +60,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
